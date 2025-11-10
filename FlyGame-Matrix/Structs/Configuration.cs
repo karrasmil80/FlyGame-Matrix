bd2e9e49142978d2a3861fc87b90a0de8f58bc52
Pablo Zuil <zupablo3@gmail.com>
2025-11-10 15:46:11 +0100

    Añadidos los parametros a las structs

---
+++ FlyGame-Matrix/Structs/Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace FlyGame_Matrix.Structs {
+    public struct Configuration {
+        public int intentos;
+        public int[,] Size;
+    }
+}
---
+++ FlyGame-Matrix/Structs/HitInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace FlyGame_Matrix.Structs {
+    public struct HitInfo {
+        public string Almost;
+        public string Goal;
+        public string Miss;
+    }
+}
